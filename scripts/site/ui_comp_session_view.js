897ad3c37595b0590cbb751ec56652cebd53add7
gdc-search <gdc-search@outlook.com>
2025-11-19 19:34:07 +0800

    remove comment section.

--- scripts/site/ui_comp_session_view.js
+++ scripts/site/ui_comp_session_view.js
@@ -1,41 +1,3 @@
-const uiCompSessionVideoComment = Vue.extend({
-	template: "#ui-comp-template-session-video-comment",
-	props: {
-		sessionItem: Object
-	},
-	computed: {
-		commentURL: function () {
-			const url = 'https://gdc-search.github.io/'
-				+ this.sessionItem.year + '/'
-				+ this.sessionItem.shortcat + '/'
-				+ this.sessionItem.vid;
-			return url;
-		}
-
-	},
-	mounted: function() {
-		document.title = this.sessionItem.title;
-		let b = document.createElement('script');
-		b.setAttribute('data-repo', "gdc-search/gdc-search.github.io");
-		b.setAttribute('data-repo-id', "R_kgDOL6GVLg");
-		b.setAttribute('data-category', "General");
-		b.setAttribute('data-category-id', "DIC_kwDOL6GVLs4CfSZS");
-		b.setAttribute('data-mapping', "title");
-		b.setAttribute('data-strict', "0");
-		b.setAttribute('data-reactions-enabled', "1");
-		b.setAttribute('data-emit-metadata', "0");
-		b.setAttribute('data-input-position', "top");
-		b.setAttribute('data-theme', "light");
-		b.setAttribute('data-lang', "en");
-		b.setAttribute('data-loading', "lazy");
-		b.async = true;
-		b.src = 'https://giscus.app/client.js';
-		b.crossorigin = "anonymous";
-		document.getElementsByClassName('remote_comments_panel')[0].appendChild(b)
-		
-	}
-});
-
 const uiCompVideoDescriptionPanel = Vue.extend({
 	template: "#ui-comp-template-video-desc-panel",
 	props: {
@@ -57,8 +19,7 @@ const uiCompSessionView = Vue.extend({
 	components: {
 		"local-video-player": uiCompLocalVideoPlayer,
 		"remote-video-player": uiCompRemoteVideoPlayer,
-		"video-desc-panel": uiCompVideoDescriptionPanel,
-		"session-video-comment": uiCompSessionVideoComment
+		"video-desc-panel": uiCompVideoDescriptionPanel
 	},
 	methods: {
 		popupSliderWindow: function (slider_url) {
